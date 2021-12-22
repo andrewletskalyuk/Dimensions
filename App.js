c410381886494b2ba8a31c980c9e7022eee68edf
andrewletskalyuk <andrewletskalyuk@gmail.com>
2021-12-22 15:34:27 +0200

    22/12/2021 - practise and practise again.

--- App.js
+++ App.js
@@ -10,6 +10,8 @@ import {
   //useDimensions, //
   useDeviceOrientation //
 } from '@react-native-community/hooks';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
   const { landscape } = useDeviceOrientation();
@@ -22,47 +24,49 @@ export default function App() {
     //   }}
     //   ></View>
     // </SafeAreaView>
-    <View
-      style={{
-        backgroundColor: '#fff',
-        flex: 1,
-        flexDirection: 'row', //це головний елемент форматування
-        justifyContent: 'center', //це другий за значимостю доступ
-        alignItems: 'center',
-        //flexDirection: 'row-reverse'
-        //flexDirection: 'column-reverse',
-        alignContent: 'center', //пр
-        flexWrap: 'wrap', //перенос контейнера якщо він не влазить в один радок
-      }}
-    >
-      <View style={{
-        backgroundColor: 'dodgerblue',
-        // flex: 2,
-        height: 100,
-        width: 100,
-        //alignSelf: 'flex-start'
-        // flexBasis: 100, // width or height
-        // flexGrow: 1,
-      }}
-      />
-      <View style={{
-        backgroundColor: 'gold',
-        //flex: 1
-        width: 100,
-        height: 100,
-        // top: 20,
-        // left: 20,
-        // position: 'relative'
-      }}
-      />
-      <View style={{
-        backgroundColor: 'tomato',
-        //flex: 1
-        width: 100,
-        height: 100
-      }}
-      />
-    </View>
+    // <View
+    //   style={{
+    //     backgroundColor: '#fff',
+    //     flex: 1,
+    //     flexDirection: 'row', //це головний елемент форматування
+    //     justifyContent: 'center', //це другий за значимостю доступ
+    //     alignItems: 'center',
+    //     //flexDirection: 'row-reverse'
+    //     //flexDirection: 'column-reverse',
+    //     alignContent: 'center', //пр
+    //     flexWrap: 'wrap', //перенос контейнера якщо він не влазить в один радок
+    //   }}
+    // >
+    //   <View style={{
+    //     backgroundColor: 'dodgerblue',
+    //     // flex: 2,
+    //     height: 100,
+    //     width: 100,
+    //     //alignSelf: 'flex-start'
+    //     // flexBasis: 100, // width or height
+    //     // flexGrow: 1,
+    //   }}
+    //   />
+    //   <View style={{
+    //     backgroundColor: 'gold',
+    //     //flex: 1
+    //     width: 100,
+    //     height: 100,
+    //     // top: 20,
+    //     // left: 20,
+    //     // position: 'relative'
+    //   }}
+    //   />
+    //   <View style={{
+    //     backgroundColor: 'tomato',
+    //     //flex: 1
+    //     width: 100,
+    //     height: 100
+    //   }}
+    //   />
+    // </View>
+    //<WelcomeScreen/>
+    <ViewImageScreen />
   );
 }
 
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,17 +1,48 @@
 import React from 'react';
-import { ImageBackground } from 'react-native';
+import { Image, ImageBackground, StyleSheet, Text } from 'react-native';
+import { View } from 'react-native';
 
 function WelcomeScreen(props) {
     return (
         <ImageBackground
+            style={styles.background}
             source={require('../assets/background.jpg')}
         >
+            <View style={styles.logoContainer}>
+                <Image style={styles.logo} source={require('../assets/logo-red.png')} />
+                <Text>Віра рятує нас</Text>
+            </View>
+            <View style={styles.loginButton}></View>
+            <View style={styles.registerButton}></View>
         </ImageBackground>
     );
 }
 
 const styles = StyleSheet.create({
-    
+    background: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+    },
+    loginButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: '#DAF7A6'
+    },
+    logo: {
+        width: 150,
+        height: 100,
+    },
+    logoContainer: {
+        position: 'absolute',
+        top: 70,
+        alignItems: "center"
+    },
+    registerButton: {
+        width: '100%',
+        height: 70,
+        backgroundColor: '#BEC438'
+    },
 })
 
 export default WelcomeScreen;
